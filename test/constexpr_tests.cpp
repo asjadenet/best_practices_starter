ab4ca6150830cc32bfb98417e9699498a828c15e
Tiit <tiit@infokiir.ee>
2023-10-04 14:47:25 +0300

    let's try to test only not macos.

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,20 +1,15 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <best_practices_starter/sample_library.hpp>
+
+#ifndef __MACH__
 TEST_CASE("Factorials are computed with constexpr", "[factorial]")
 {
-	#ifdef __APPLE__
-	  STATIC_REQUIRE(factorial_constexpr(0) == 1); // cppcheck-suppress knownConditionTrueFalse
-	  STATIC_REQUIRE(factorial_constexpr(1) == 1); // cppcheck-suppress knownConditionTrueFalse
-	  STATIC_REQUIRE(factorial_constexpr(2) == 2); // cppcheck-suppress knownConditionTrueFalse
-	  STATIC_REQUIRE(factorial_constexpr(3) == 6); // cppcheck-suppress knownConditionTrueFalse
-	  STATIC_REQUIRE(factorial_constexpr(10) == 3628800); // cppcheck-suppress knownConditionTrueFalse	
-	#else
+
 	  STATIC_REQUIRE(factorial_constexpr(0) == 1);
 	  STATIC_REQUIRE(factorial_constexpr(1) == 1);
 	  STATIC_REQUIRE(factorial_constexpr(2) == 2);
 	  STATIC_REQUIRE(factorial_constexpr(3) == 6);
 	  STATIC_REQUIRE(factorial_constexpr(10) == 3628800);
-	#endif
-
 }
+#endif
